e35d1ca34c7f43f408894ede28ddbb78a9605b01
Manishkmr406 <manishkmr406@gmail.com>
2023-07-18 16:48:28 +0530

    firebase auth and redux done

--- src/components/header/QHeader.js
+++ src/components/header/QHeader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./QHeader.css";
 import HomeIcon from '@mui/icons-material/Home';
 import FeaturedPlayListOutlinedIcon from '@mui/icons-material/FeaturedPlayListOutlined';
@@ -7,9 +7,34 @@ import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 import SearchIcon from '@mui/icons-material/Search';
 import LanguageIcon from '@mui/icons-material/Language';
-import { Avatar, Button } from '@mui/material';
+import { Avatar, Button, Input } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../features/userSlice';
+import { signOut } from 'firebase/auth';
+import db,{ auth } from '../../firebase';
+import Modal from "react-modal";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import LinkIcon from '@mui/icons-material/Link';
+import { FirebaseError } from 'firebase/app';
 
 function QHeader() {
+  const user=useSelector(selectUser);
+
+  const [IsmodalOpen, setIsModalOpen] = useState(false);
+  const [input, setInput] = useState("");
+  const [inputUrl, setInputUrl] = useState("");
+  const questionName = input;
+
+  const handleQuestion = (e) => {
+    e.preventDefault();
+    setIsModalOpen(false);
+
+    
+
+    setInput("");
+    setInputUrl("");
+  };
+
   return (
     <div className="qHeader">
     <div className="qHeader__logo">
@@ -41,11 +66,81 @@ function QHeader() {
       </div>
       <div className="qHeader__Rem">
         <div className="qHeader__avatar">
-          <Avatar /> 
+          <Avatar 
+            onClick={()=> signOut(auth)}
+            className="Avatar"
+            src={
+              user.photo
+                ? user.photo
+                : "https://images-platform.99static.com//_QXV_u2KU7-ihGjWZVHQb5d-yVM=/238x1326:821x1909/fit-in/500x500/99designs-contests-attachments/119/119362/attachment_119362573"
+            }
+          /> 
         </div>
         <LanguageIcon />
-        <Button>Add Question</Button>
-        
+        <Button onClick={() => setIsModalOpen(true)}>Add Question</Button>
+        <Modal
+          isOpen={IsmodalOpen}
+          onRequestClose={() => setIsModalOpen(false)}
+          shouldCloseOnOverlayClick={false}
+          style={{
+            overlay: {
+              width: 700,
+              height: 600,
+              backgroundColor: "rgba(0,0,0,0.8)",
+              zIndex: "1000",
+              top: "50%",
+              left: "50%",
+              marginTop: "-300px",
+              marginLeft: "-350px",
+            },
+          }}
+        >
+          <div className="modal__title">
+            <h5>Add Question</h5>
+            <h5>Share Link</h5>
+          </div>
+          <div className="modal__info">
+            <Avatar
+              className="avatar"
+              src={
+                user.photo
+                  ? user.photo
+                  : "https://images-platform.99static.com//_QXV_u2KU7-ihGjWZVHQb5d-yVM=/238x1326:821x1909/fit-in/500x500/99designs-contests-attachments/119/119362/attachment_119362573"
+              }
+            />
+            <p>{user.disPlayName ? user.disPlayName : user.email} asked</p>
+            <div className="modal__scope">
+              <PeopleAltOutlinedIcon />
+              <p>Public</p>
+              <ExpandMoreIcon />
+            </div>
+          </div>
+          <div className="modal__Field">
+            <Input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+              placeholder="Start your question with 'What', 'How', 'Why', etc. "
+            />
+            <div className="modal__fieldLink">
+              <LinkIcon />
+              <input
+                value={inputUrl}
+                onChange={(e) => setInputUrl(e.target.value)}
+                type="text"
+                placeholder="Optional: inclue a link that gives context"
+              ></input>
+            </div>
+          </div>
+          <div className="modal__buttons">
+            <button className="cancle" onClick={() => setIsModalOpen(false)}>
+              Cancel
+            </button>
+            <button type="sumbit" onClick={handleQuestion} className="add">
+              Add Question
+            </button>
+          </div>
+        </Modal>
       </div>
     </div>
   )
